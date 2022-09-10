07f4724605a4be4294e908aa3f197140e2434faa
Jacob Strader <11006561@live.mercer.edu>
2022-09-10 02:50:03 -0400

    create Xavier/He weight initialization functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,11 @@ mod errors;
 use errors::InvalidGrayscaleImageError;
 use image::{DynamicImage, GenericImageView};
 use nalgebra::DMatrix;
+use rand::{distributions::Uniform, Rng};
+use rand_distr::StandardNormal;
 
 /// Log information of the current image.
-pub fn log_image_info(path: &str, image: &DynamicImage) {
+pub fn __log_image_info(path: &str, image: &DynamicImage) {
     println!("Image Path: {}", path);
     println!("Dimensions: {:?}", image.dimensions());
     println!("Pixel Count: {}", image.pixels().count());
@@ -40,5 +42,106 @@ pub fn get_pixel_matrix(image: &DynamicImage) -> Result<DMatrix<i16>, InvalidGra
             gray_image.pixels().map(|p| p.0[0] as i16),
         )),
         _ => Err(InvalidGrayscaleImageError),
+    }
+}
+
+/// Generate set of weights using Xavier initializing.
+///
+/// # Arguments
+/// *input_size* - The size of the input layer
+/// *layer_1_size* - The number of
+///
+fn get_xavier_weight_matrix(input_size: usize, output_size: usize) -> DMatrix<f64> {
+    // rows, columns
+    let dims = (output_size, input_size + 1);
+
+    let (upper, lower) = (
+        1_f64 / (input_size as f64).sqrt(),
+        -1_f64 / (input_size as f64).sqrt(),
+    );
+
+    DMatrix::from_iterator(
+        dims.0,
+        dims.1,
+        rand::thread_rng()
+            .sample_iter(Uniform::new_inclusive(lower, upper))
+            .take(dims.0 * dims.1),
+    )
+}
+
+/// Generate set of weights using He initialization
+///
+/// # Arguments
+/// *input_size* - The size of the input layer
+///
+fn get_he_weight_matrix(input_size: usize, output_size: usize) -> DMatrix<f64> {
+    // rows, columns
+    let dims = (output_size, input_size + 1);
+
+    let std: f64 = (2_f64 / input_size as f64).sqrt();
+
+    DMatrix::from_iterator(
+        dims.0,
+        dims.1,
+        rand::thread_rng()
+            .sample_iter(StandardNormal)
+            .take(dims.0 * dims.1)
+            .map(|n: f64| n * std),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    /// Test obtaining weight values
+    fn xavier_init() {
+        let (input_size, output_size) = (100, 32);
+        assert_eq!(
+            get_xavier_weight_matrix(input_size, output_size).len(),
+            output_size * (input_size + 1)
+        );
+    }
+
+    #[test]
+    /// Print sample matrix from init_weight
+    fn print_xavier_init() {
+        let (input_size, output_size) = (5, 10);
+        println!("{}", get_xavier_weight_matrix(input_size, output_size));
+    }
+
+    #[test]
+    /// Verify no value in the weight matrix is outside the distribution range
+    fn check_weight_in_range() {
+        let (input_size, output_size) = (30, 300);
+        let m = get_xavier_weight_matrix(input_size, output_size);
+
+        let (upper, lower) = (
+            1_f64 / (input_size as f64).sqrt(),
+            -1_f64 / (input_size as f64).sqrt(),
+        );
+
+        for x in m.into_iter() {
+            assert!(*x >= lower && *x <= upper);
+        }
+    }
+
+    #[test]
+    /// Test obtaining weight values
+    fn he_init() {
+        let (input_size, output_size) = (100, 32);
+        assert_eq!(
+            get_he_weight_matrix(input_size, output_size).len(),
+            output_size * (input_size + 1)
+        );
+    }
+
+    #[test]
+    /// Print sample matrix from init_weight
+    fn print_he_init() {
+        let (input_size, output_size) = (5, 10);
+        println!("{}", get_he_weight_matrix(input_size, output_size));
     }
 }
